4038eefdd9ab603f02fb086b7d26ba9cac233ec0
LaNice Powell <lanicemp@aol.com>
2019-07-11 21:38:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,17 +3,17 @@ def nyc_pigeon_organizer(pegion_data)
   organized_data = {}
   pegion_data.each do  |category, options|
     # each is an iterator and a loop is ()
-    options.each do |option, names|
+    options.each do |option_color, names|
       names.each do |name|
        if organized_data[name]
+         binding.pry
          organized_data[:color][]
      else
         organized_data[name] = {:color => [], :gender => [], :lives => []}
     end                            
-    binding.pry
+    
       end
     end
-    
     #pegion_name = {:pegion_name => ["Theo", "Peter Jr.","Ms K", "Queenie", "Andrew", "Alex", "Lucky"]}
     #if data{color, gender, lives}
      #binding.pry 
