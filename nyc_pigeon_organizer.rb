639b438611ab1b91da988a0b9ac72df69b70bc88
LaNice Powell <lanicemp@aol.com>
2019-07-11 20:19:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(pegion_data)
   pegion_data.each do  |data|
-    if data{color, gender, lives}
+    #if data{color, gender, lives}
      binding.pry 
     # write your code here!
 end
