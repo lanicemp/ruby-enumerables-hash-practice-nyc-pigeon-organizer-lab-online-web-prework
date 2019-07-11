7c1ac38c85c6cc395d56afe5c2dde61258f0f62f
LaNice Powell <lanicemp@aol.com>
2019-07-11 22:01:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+def nyc_pigeon_organizer(pegion_data)
+  organized_data = {}
+  pegion_data.each do  |category, options|
+    # each is an iterator and a loop is ()
+    options.each do |option_color, names|
+      names.each do |name|
+       if organized_data[name]
+         #binding.pry
+         organized_data[name][category] ||= []
+         #||= means or equal if the element to the left already has a value we fo not touch it. 
+         organized_data[name][category] << option_color.to_s
+         #to_s converts symbol into string
+     else
+        organized_data[name] = {:color => [], :gender => [], :lives => []}
+        organized_data[name][category] << option_color.to_s
+    end                            
+    
+      end
+    end
+    #pegion_name = {:pegion_name => ["Theo", "Peter Jr.","Ms K", "Queenie", "Andrew", "Alex", "Lucky"]}
+    #if data{color, gender, lives}
+     #binding.pry 
+    # write your code here!
 end
+organized_data
+end
+
