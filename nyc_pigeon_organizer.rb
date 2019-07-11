57d799ef41c21870ffef3f1caf55ff187692969d
LaNice Powell <lanicemp@aol.com>
2019-07-11 20:44:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(pegion_data)
   pegion_data.each do  |pegion_name|
-    pegion_name = {:pegion_name => ["Theo", "Peter Jr.","Ms K", "Queenie", "Andrew", "Alex" "Lucky"]}
+    pegion_name = {:pegion_name => ["Theo", "Peter Jr.","Ms K", "Queenie", "Andrew", "Alex", "Lucky"]}
     #if data{color, gender, lives}
      binding.pry 
     # write your code here!
